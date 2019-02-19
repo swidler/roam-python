d5db2256e49475fb0d4ac1f12823a6130b8f61db
Nomi <swidler@gmail.com>
2019-02-19 10:13:09 +0200

    Added standardize_region function

--- tools.py
+++ tools.py
@@ -19,3 +19,24 @@ def nanconv(arr, operation):
                 merged = (arr[i]+arr[i+1])*factor #num + num = (num + num) * factor. yields float result, with
                 result.append(merged)             #sometimes strange precision (eg 0.8200000000000001)
     return result
+
+def standardize_region(region):
+    import re
+    if isinstance(region, dict):
+        return region
+    fields = re.split(" |:|-", region)
+    chrom = fields[0]
+    start = fields[1]
+    end = fields[2]
+    start = start.replace(",","")
+    end = end.replace(",","")
+    std_region = {
+            "chrom":chrom,
+            "start":start,
+            "end":end
+            }
+    return std_region
+if __name__ == "__main__":
+    reg = "chr1:234,543,678-234,567,890"
+    new_reg = standardize_region(reg)
+    print(new_reg)
