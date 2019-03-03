0ea8d82c7031021155f83ab7e9e4111474a1255a
Nomi <swidler@gmail.com>
2019-03-03 13:25:35 +0200

    added method to parse input file

--- gcoordinates.py
+++ gcoordinates.py
@@ -18,3 +18,31 @@ class Gcoordinates(Chrom):
 
     def __repr__(self): #defines print of object
         return "name: %s\ndescription: %s\nspecies: %s\nreference: %s\nmetadata: %s\nchr_names: %s\ncoords: %s\nstrand: %s\nno_chrs: %s" % (self.name, self.description, self.species, self.reference, self.metadata, self.chr_names, self.coords, self.strand, self.no_chrs)
+
+    def parse_infile(self, infile):
+        i = 0
+        temp = []
+        with open(infile, "rt") as gcfile:
+            for line in gcfile:
+                line = line.rstrip("\n") #remove line feeds
+                if i == 0:
+                    header = line #first line is header
+                    self.description = header
+                    fields = header.split(" ")
+                    ref = fields[2]
+                    ref = ref.rstrip(")")
+                    ref = ref.lstrip("(")
+                    self.reference = ref
+                    self.species = fields[0]
+                    self.name = fields[1]
+                elif i > 1: #2nd line is blank, rest are chroms
+                    fields = line.split("\t")
+                    self.chr_names.append(fields[0])
+                    coords = fields[1].split(",")
+                    coords = [int(x) if x.isdigit else np.nan for x in coords] #convert numbers to int, leave nans
+                    temp.append(np.asarray(coords))
+                i += 1
+        self.coords = np.asarray(temp)
+        self.no_chrs = len(self.coords) #reassign chrom num based on new info
+
+
