28e018d3c74bdb1b76bb30e58699fa8a706cd469
Nomi <swidler@gmail.com>
2019-03-05 16:24:41 +0200

    gc now loads from binary instead of parsing text

--- test_classes.py
+++ test_classes.py
@@ -2,12 +2,15 @@
 
 import mmsample as m
 import gcoordinates as g
+import tools as t
 
 mms = m.Mmsample()
 mms.parse_infile("/mnt/x/bone_5_short.txt")
 #gc = g.Gcoordinates(chr_names=["chr2", "chr3", "chr4", "chr8"], coords=[[234,453, 563], [222, 223, 224], [121, 122, 123, 124, 125, 126, 455, 456, 457, 458], [123, 234, 235]])
-gc = g.Gcoordinates()
-gc.parse_infile("/mnt/x/cpg_coords.txt")
-region = "chr4:10100-10330"
+#gc = g.Gcoordinates()
+#gc.parse_infile("/mnt/x/cpg_coords.txt")
+infile = "objects/cpg_coords"
+gc = t.load_object(infile)
+region = "chr2:10100-10460"
 meth = mms.region_methylation(region, gc)
 print(f"meth is {meth}")
