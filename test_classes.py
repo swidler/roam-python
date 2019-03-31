b3ba484b5be4a9ad9bef23cadb421b3c56b69c7c
Nomi <swidler@gmail.com>
2019-03-31 10:36:08 +0300

    tests ams region_methylation

--- test_classes.py
+++ test_classes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import mmsample as m
+import amsample as a
 import gcoordinates as g
 import tools as t
 
@@ -11,10 +12,17 @@ import tools as t
 #gc = g.Gcoordinates(chr_names=["chr2", "chr3", "chr4", "chr8"], coords=[[234,453, 563], [222, 223, 224], [121, 122, 123, 124, 125, 126, 455, 456, 457, 458], [123, 234, 235]])
 #gc = g.Gcoordinates()
 #gc.parse_infile("/mnt/x/cpg_coords.txt")
-infile = "objects/bone_5"
-mms = t.load_object(infile)
+#infile = "objects/bone_5"
+#mms = t.load_object(infile)
+#ams = a.Amsample()
+#ams.parse_infile("../../I1116.txt")
+#outfile = "objects/I1116"
+#t.save_object(outfile, ams)
+infile = "objects/I1116"
+ams = t.load_object(infile)
 infile = "objects/cpg_coords"
 gc = t.load_object(infile)
-region = "chr2:10100-10460"
-meth = mms.region_methylation(region, gc)
+region = "chr5:89887654-987645387"
+#meth = mms.region_methylation(region, gc)
+meth = ams.region_methylation(region, gc)
 print(f"meth is {meth}")
