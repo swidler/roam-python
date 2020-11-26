059d68bc1a91eaa46c7a6a6529ee0426308d64dc
swidler <swidler@gmail.com>
2020-11-26 13:52:56 +0200

    added testing for gintervals

--- test_classes.py
+++ test_classes.py
@@ -4,11 +4,12 @@ import mmsample as m
 import amsample as a
 import gcoordinates as g
 import tools as t
+import gintervals as gint
 
-mms = m.Mmsample()
-mms.parse_infile("/mnt/x/ref_bone5_take2.txt")
-outfile = "objects/ref_bone5_take2"
-t.save_object(outfile, mms)
+#mms = m.Mmsample()
+#mms.parse_infile("/mnt/x/ref_bone5_take2.txt")
+#outfile = "objects/ref_bone5_take2"
+#t.save_object(outfile, mms)
 #gc = g.Gcoordinates(chr_names=["chr2", "chr3", "chr4", "chr8"], coords=[[234,453, 563], [222, 223, 224], [121, 122, 123, 124, 125, 126, 455, 456, 457, 458], [123, 234, 235]])
 #gc = g.Gcoordinates()
 #gc.parse_infile("/mnt/x/cpg_coords.txt")
@@ -26,3 +27,5 @@ t.save_object(outfile, mms)
 #meth = mms.region_methylation(region, gc)
 #meth = ams.region_methylation(region, gc)
 #print(f"meth is {meth}")
+chroms = ["chr1", "chr2", "chr3"]
+gin = gint.Gintervals(chr_names=chroms)
