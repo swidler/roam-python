0d4010bf708b6deef9483d8ac81147572f0eda6a
swidler <swidler@gmail.com>
2022-03-01 15:20:22 +0200

    added var for data base dir and added create_files stage

--- config_DMR.py
+++ config_DMR.py
@@ -1,13 +1,16 @@
 # dirs
 object_dir = "directory for pickled objects"
 # eg object_dir = "objects/"
+data_dir = "directory for data files from RoAM process"
+# eg data_dir = "data/python_dumps/"
 dump_dir = "directory for output txt files and pics"
-# eg dump_dir = "data/python_dumps/DMR/"
+# eg dump_dir = data_dir + "DMR/"
+
 
 # files
 gc_object = object_dir + "cpg_coords.P"  # CpG file
-gene_file = "../../UCSC_Genes_sorted.txt"  # text file  sorted on the cmdline: sort -k1,1n -k2,2n -k3,3n fn>fn_sorted
-cgi_file = "../../UCSC_CGIs.txt"  # CGI file
+gene_file = "UCSC_Genes_sorted.txt"  # text file  sorted on the cmdline: sort -k1,1n -k2,2n -k3,3n fn>fn_sorted
+cgi_file = "UCSC_CGIs.txt"  # CGI file
 DMR_obj_infile = "pickled DMR file for use in permutations and plots"
 # eg DMR_obj_infile = object_dir + "DMR_obj_27-02-2022_10.33"
 DMP_obj_infile = "pickled DMR permutation file for use in permutstat"
@@ -17,10 +20,10 @@ DMP_obj_infile = "pickled DMR permutation file for use in permutstat"
 templ = "template to match any extra text in sample file"
 # eg templ = "filt_"
 
-scripts = ["DMR", "permute", "permutstat", "plotmethylation"] # scripts in the process. Change to reflect what you want to run.
-# DMR runs the groupDMR process and the annotation. It can be run independently or in conjunction with permutation scripts.
-# Permutation scripts (permute runs permutations, permutstat calculates permutation statistics) can be run independently as 
-# long as they have a DMR file to work with. The plotmethylation script also requires a DMR file.
+stages = ["create_files", "DMR", "permute", "permutstat", "plotmethylation"] # stages in the process. Change to reflect what you want to run.
+# DMR runs the groupDMR process and the annotation. It can be run independently or in conjunction with permutation stages.
+# Permutation stages (permute runs permutations, permutstat calculates permutation statistics) can be run independently as 
+# long as they have a DMR file to work with. The plotmethylation method also requires a DMR file.
 
 
 # groupDMRs vars
