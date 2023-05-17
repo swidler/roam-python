637b5acda969144a762125638d64c8e3f97eac55
swidler <swidler@gmail.com>
2023-05-17 10:59:22 +0300

    added command line arguments for required params, added params to bam processing, added required params to function call

--- run_roam.py
+++ run_roam.py
@@ -6,8 +6,22 @@ import tools as t
 #from config import *
 import config as cfg
 import glob
+import argparse
 
-def roam_pipeline(filename=cfg.filename, name=cfg.name, abbrev=cfg.abbrev):
+# first 4 (required) params from config can be specified on the command line
+argParser = argparse.ArgumentParser()
+argParser.add_argument("-f", "--filename", help="path of bam input file")
+argParser.add_argument("-l", "--library", help="single or double stranded")
+argParser.add_argument("-n", "--name", help="sample name")
+argParser.add_argument("-a", "--abbrev", help="sample abbreviation")
+
+args = argParser.parse_args()
+fn = args.filename if args.filename else cfg.filename
+name = args.name if args.name else cfg.name
+abbrev = args.abbrev if args.abbrev else cfg.abbrev
+library = args.library if args.library else cfg.library
+
+def roam_pipeline(filename=cfg.filename, name=cfg.name, abbrev=cfg.abbrev, library=cfg.library):
     #create Amsample object
     ams = a.Amsample(name=name, abbrev=abbrev)
     # eg: ams = a.Amsample(name="Ust_Ishim", abbrev="Ust")
@@ -15,7 +29,7 @@ def roam_pipeline(filename=cfg.filename, name=cfg.name, abbrev=cfg.abbrev):
     stage = stages[0]
     if stage == "bam":
        #populate object from bam file
-       ams.bam_to_am(filename=filename, library=cfg.library, chr_lengths=cfg.chr_lengths, species=cfg.species, trim_ends=cfg.trim_ends, chroms=cfg.chroms, filedir=cfg.filedir, file_per_chrom=cfg.file_per_chrom)
+       ams.bam_to_am(filename=filename, library=library, chr_lengths=cfg.chr_lengths, species=cfg.species, trim_ends=cfg.trim_ends, chroms=cfg.chroms, filedir=cfg.filedir, file_per_chrom=cfg.file_per_chrom, mapq_thresh=cfg.mapq_thresh, qual_thresh=cfg.qual_thresh)
        # eg: ams.bam_to_am(filename="../../ust_ishim.bam", library="single", chr_lengths=ust_chr_lengths, genome_seq="../../hg19.fa.gz", species="Homo sapiens")
        stages = stages[1:]  # remove bam stage from list 
     else:
@@ -52,4 +66,4 @@ if cfg.filedir and not cfg.file_per_chrom:
         name = abbrev = fn.split("/")[-1].split(".")[0]
         roam_pipeline(filename=fn, name=name, abbrev=abbrev)
 else:
-    roam_pipeline(filename=cfg.filename)
+    roam_pipeline(filename=fn, name=name, abbrev=abbrev, library=library)
