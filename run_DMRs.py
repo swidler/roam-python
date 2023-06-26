a3fe62cbe8fbcf44106a90f03d126ca8b789ca10
swidler <swidler@gmail.com>
2023-06-26 09:00:43 +0300

    added command line params; cleaned up script

--- run_DMRs.py
+++ run_DMRs.py
@@ -6,13 +6,60 @@ import mmsample as m
 import tools as t
 import datetime
 import copy
-from config_DMR import *  # import as cfg for disambiguation?
-import config as cfg
+import config_DMR as cfg  
+import config as rcfg
+import argparse
+import sys
 
+# params from config can be specified on the command line
+argParser = argparse.ArgumentParser()
+argParser.add_argument("-s", "--samples", nargs="+", help="sample names")
+argParser.add_argument("-g", "--groups", nargs="+", help="group names--should correspond with samples")
+argParser.add_argument("-o", "--object_dir", help="directory for pickled objects")
+argParser.add_argument("-d", "--data_dir", help="directory for data files from RoAM process")
+argParser.add_argument("-du", "--dump_dir", help="directory for output txt files and pics")
+argParser.add_argument("-gc", "--gc_file", help="CpG file")
+argParser.add_argument("-ge", "--gene_file", help="sorted text file with genes")
+argParser.add_argument("-cg", "--cgi_file", help="CGI file")
+argParser.add_argument("-di", "--dmr_infile", help="pickled DMR file for use in permutations and plots")
+argParser.add_argument("-dpi", "--dmp_infile", help="pickled DMR permutation file for use in permutstat")
+argParser.add_argument("-t", "--templ", help="template to match any extra text in sample filename")
+argParser.add_argument("-st", "--stages", nargs="+", help="stages of process to be run")
+argParser.add_argument("-de", "--delta", help="")
+argParser.add_argument("-mc", "--min_cpgs", help="")
+argParser.add_argument("-mf", "--min_finite", nargs="+", help="")
+argParser.add_argument("-w", "--win_size", help="")
+argParser.add_argument("-l", "--lcf", help="")
+argParser.add_argument("-p", "--permutations", help="number of permutations to run")
+argParser.add_argument("-dmi", "--dmr_idx", help="index of DMR")
+argParser.add_argument("-dmc", "--dmr_chrom", help="chromosome of DMR")
+argParser.add_argument("-b", "--bismark", help=".cov or .bedGraph file for modern genome")
+
+args = argParser.parse_args()
+keys = [x for x in vars(args).keys() if vars(args)[x] != None]
+vals = [vars(args)[x] for x in keys]
+parameters = dict(zip(keys, vals))
+samples = parameters["samples"] if "samples" in parameters else cfg.samples
+templ = parameters["templ"] if "templ" in parameters else cfg.templ
+object_dir = parameters["object_dir"] if "object_dir" in parameters else cfg.object_dir
+stages = parameters["stages"] if "stages" in parameters else cfg.stages
+gc_object = parameters["gc_file"] if "gc_file" in parameters else cfg.gc_object
+min_fin = parameters["min_finite"] if "min_finite" in parameters else cfg.min_fin
+win_size = parameters["win_size"] if "win_size" in parameters else cfg.win_size
+lcf = parameters["lcf"] if "lcf" in parameters else cfg.lcf
+group_names = parameters["groups"] if "groups" in parameters else cfg.group_names
+min_CpGs = parameters["min_cpgs"] if "min_cpgs" in parameters else cfg.min_CpGs
+delta = parameters["delta"] if "delta" in parameters else cfg.delta
+bismark_infile = parameters["bismark"] if "bismark" in parameters else rcfg.bismark_infile
+gene_file = parameters["gene_file"] if "gene_file" in parameters else cfg.gene_file
+cgi_file = parameters["cgi_file"] if "cgi_file" in parameters else cfg.cgi_file
+dump_dir = parameters["dump_dir"] if "dump_dir" in parameters else cfg.dump_dir
+# add param for permutations in permute?
 
 time = datetime.datetime.now()
 time = time.strftime("%d-%m-%Y_%H.%M")
 if "create_files" in stages:
+    data_dir = parameters["data_dir"] if "data_dir" in parameters else cfg.data_dir
     for sample in samples:
         ams = a.Amsample()
         filename = data_dir + sample + templ + ".txt"
@@ -35,28 +82,22 @@ if "DMR" in stages or "permute" in stages or "plot" in stages:
 if "DMR" in stages:
     dms = d.DMRs()
 else:
+    DMR_obj_infile = parameters["dmr_infile"] if "dmr_infile" in parameters else cfg.DMR_obj_infile
     dms = t.load_object(DMR_obj_infile)
 if "DMR" in stages:
     import cProfile
     min_finite = min_fin[:]
     #cProfile.run("(qt_up, qt_down) = dms.groupDMRs(samples=samplist, sample_groups=group_names, coord=gc, chroms=chr_names, min_finite=[0.8,0.8], min_CpGs=min_CpGs, delta=delta)", "data/logs/DMR_profile")
     (qt_up, qt_down) = dms.groupDMRs(win_size=win_size, lcf=lcf, samples=samplist, sample_groups=group_names, coord=gc, chroms=chr_names, min_finite=min_finite, min_CpGs=min_CpGs, delta=delta)
-    #dms.annotate(gene_file, cgi_file)  # move annotation to after fdr? or call here only when dmr, but not fdr in stages.
     #cProfile.run("dms.annotate(gene_file, cgi_file)", "data/logs/annotate_profile")
-    fname = dump_dir + f"DMRs_{time}.txt"
-    #fname = dump_dir + "python_DMRs.txt"
-    #with open(fname, "w") as fid:
-    #    for chrom in range(len(dms.chromosomes)):
-    #        for dmr in range(len(dms.cDMRs[chrom].gen_start)):
-    #            fid.write(f"{chrom+1}\t{dms.cDMRs[chrom].gen_start[dmr]}\t{dms.cDMRs[chrom].gen_end[dmr]}\n")
-
-    #dms.dump_DMR(fname)  # dump doesn't currently work w/out annotate
+    
     t.save_object(f"{object_dir}DMR_obj_{time}", dms) 
 if "fdr" in stages:
     #create Mmsample object
+    sim_permutations = parameters["permutations"] if "permutations" in parameters else cfg.sim_permutations
     mms = m.Mmsample()
-    if cfg.bismark_infile:
-        mms.create_mms_from_bismark_file()
+    if bismark_infile:
+        mms.create_mms_from_bismark_file(bismark_infile, gc_object, rcfg.mod_name, rcfg.mod_abbrev, rcfg.mod_species, rcfg.mod_ref, rcfg.mod_method)
     else:
         mms.create_mms_from_text_file()
     samplist = []  # if dmr in stages, samplist already loaded
@@ -67,7 +108,6 @@ if "fdr" in stages:
         print(f"loading sample {sample}")
         input_obj = t.load_object(infile)
         samplist.append(input_obj)
-    #sim_obj_list = []
     dmr_obj_list = []
     gc = t.load_object(gc_object)
     chr_names = gc.chr_names  # assumes user wants all chroms (or all but x)
@@ -75,7 +115,6 @@ if "fdr" in stages:
     for perm in range(sim_permutations):
         sim_obj_list = {}
         dmr_obj_list.append(d.DMRs())
-    #for perm in range(sim_permutations):
         for sample in samplist:
             print(f"sample {sample.name}, perm {perm}\n")
             samp_name = sample.name
@@ -87,11 +126,16 @@ if "fdr" in stages:
             sim_obj_list[samp_name] = samp_copy
             del samp_copy  # will this fix memory errors?
             #sample.dump(f"simeth_{perm}")  
-    #for perm in range(sim_permutations):
         min_finite = min_fin[:]
         dmr_obj_list[perm].groupDMRs(win_size=win_size, lcf=lcf, samples=list(sim_obj_list.values()), sample_groups=group_names, coord=gc, chroms=chr_names, min_finite=min_finite, min_CpGs=min_CpGs, delta=delta)
     adjusted_DMR = dms.adjust_params(dmr_obj_list)
     print("done")
+    
+if "DMR" in stages:
+    dms.annotate(gene_file, cgi_file)  
+    fname = dump_dir + f"DMRs_{time}.txt"
+    dms.dump_DMR(fname)  # dump doesn't currently work w/out annotate
+    
     
         
     
@@ -105,10 +149,15 @@ if "permute" in stages:
     t.save_object(f"{object_dir}DMP_obj{time}", dmp) 
 if "permutstat" in stages:
     if "permute" not in stages:
+        DMP_obj_infile = parameters["dmp_infile"] if "dmp_infile" in parameters else cfg.DMP_obj_infile
         dmp = t.load_object(DMP_obj_infile)
     pstat = dms.permutstat(dmp)
     fname = dump_dir + f"pstat_{time}.txt"
     dms.dump_pstat(pstat, fname)
+    
+DMR_chrom = parameters["dmr_chrom"] if "dmr_chrom" in parameters else cfg.DMR_chrom
+DMR_idx = parameters["dmr_idx"] if "dmr_idx" in parameters else cfg.DMR_idx
+    
 if "plotmethylation" in stages:
     fname = dump_dir + f"meth_plot_{DMR_chrom}_{DMR_idx}"
     dms.plotmethylation(DMR_chrom, DMR_idx, fname)  
