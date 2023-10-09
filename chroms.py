f940dcb736a58f1eabb8231d53f9db25b88118d5
swidler <swidler@gmail.com>
2023-10-09 10:09:33 +0300

    handling of chrom input to index method without 'chr' in name

--- chroms.py
+++ chroms.py
@@ -14,6 +14,8 @@ class Chrom:
         """
         result = []
         for chrom in chr_name:
+            if "chr" not in chrom and any("chr" in x for x in self.chr_names):  # if chr in self.chr_names, make sure input chrom has "chr"
+                chrom = "chr" + chrom
             if chrom not in self.chr_names:
                 result.append(np.nan) #returns NaN as index of nonexistent chrom
             else:
