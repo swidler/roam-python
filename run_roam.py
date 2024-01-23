c8449a6a15a03588e02cf37679be0b0a42617831
swidler <swidler@gmail.com>
2024-01-23 13:54:36 +0200

    added 3 params, improved help, added call to create_cpg_file, cleaned up

--- run_roam.py
+++ run_roam.py
@@ -9,6 +9,7 @@ import glob
 import argparse
 import sys
 import configparser as cp
+import gcoordinates as gcoord
 
 
 # params from config can be specified on the command line
@@ -25,12 +26,12 @@ argParser.add_argument("-t", "--trim", help="set True to trim ends during proces
 argParser.add_argument("-cf", "--chrom_file", help="set True for dir with one file per chromosome")
 argParser.add_argument("-m", "--mapq", help="mapping quality for read")
 argParser.add_argument("-q", "--qual", help="mapping quality for position")
-argParser.add_argument("-st", "--stages", nargs="+", help="stages of process to be run")
-argParser.add_argument("-o", "--objectdir", help="directory for saved (pickled) object files")
-argParser.add_argument("-fd", "--filedir", help="directory for multiple input files")
-argParser.add_argument("-od", "--outdir", help="output directory")
-argParser.add_argument("-ld", "--logdir", help="directory for log files")
-argParser.add_argument("-pd", "--picdir", help="directory for images")
+argParser.add_argument("-st", "--stages", nargs="+", help="stages of process to be run (bam diagnose filter drate meth)")
+argParser.add_argument("-o", "--objectdir", help="directory for saved (pickled) object files (include final /)")
+argParser.add_argument("-fd", "--filedir", help="directory for multiple input files (include final /)")
+argParser.add_argument("-od", "--outdir", help="output directory (include final /)")
+argParser.add_argument("-ld", "--logdir", help="directory for log files (include final /)")
+argParser.add_argument("-pd", "--picdir", help="directory for images (include final /)")
 argParser.add_argument("-g", "--genome", help="path for the assembly file, eg hg19.fa.gz")
 argParser.add_argument("-tf", "--text_in", help="path for the object saved in text format")
 argParser.add_argument("-mo", "--modern", help="text file for modern genome")
@@ -42,6 +43,9 @@ argParser.add_argument("-ms", "--mspecies", help="modern sample species")
 argParser.add_argument("-mr", "--mref", help="modern sample reference genome")
 argParser.add_argument("-mm", "--mmethod", help="modern sample sequencing method")
 argParser.add_argument("-bed", "--nobed", help="flag for bed file", action="store_true")
+argParser.add_argument("-cpg", "--create_cpg", help="flag for creating cpg file", action="store_true")
+argParser.add_argument("-cr", "--cpg_ref", help="reference genome assembly for CpG file")
+argParser.add_argument("-no", "--no_roam", help="flag for not running the rest of RoAM", action="store_true")
 
 args = argParser.parse_args()
 keys = [x for x in vars(args).keys() if vars(args)[x] != None]
@@ -54,7 +58,26 @@ config.read(confile)
 filedir = parameters["filedir"] if "filedir" in parameters else config["paths"]["filedir"]
 file_per_chrom = parameters["chrom_file"] if "chrom_file" in parameters else config["basic"].getboolean("file_per_chrom")
 bed = False if parameters["nobed"] else config["files"].getboolean("bed")
-                
+species = parameters["species"] if "species" in parameters else config["basic"]["species"]
+chroms = parameters["chroms"] if "chroms" in parameters else config["basic"]["chroms"].split(",")
+genome_file = parameters["genome"] if "genome" in parameters else config["files"]["genome_file"]
+object_dir = parameters["objectdir"] if "objectdir" in parameters else config["paths"]["object_dir"]
+lengths = parameters["lengths"] if "lengths" in parameters else config["basic"]["chr_lengths"].split(",")
+lengths = [int(x) for x in lengths]
+if parameters["create_cpg"]:
+    outfile = object_dir + species.replace(" ","_") + "_cpg_coords.P"
+    ref = parameters["cpg_ref"] if "cpg_ref" in parameters else config["basic"]["cpg_ref"]
+    if not ref or ref == "":
+        ref = genome_file.split("/")[-1].split(".")[0]
+    gc_obj = gcoord.Gcoordinates(chr_names=chroms, species=species, name="CpG coordinates", reference=ref)
+    gc_obj.create_cpg_file(genome_file, outfile, lengths)
+    if parameters["no_roam"]:
+        print("Finished creating CpG file. Exiting.")
+        sys.exit(0)
+    gc = outfile
+else:
+    gc = parameters["gc_file"] if "gc_file" in parameters else config["files"]["gc_object"]
+               
 #def roam_pipeline(filename=cfg.filename, name=cfg.name, abbrev=cfg.abbrev, library=cfg.library):
 def roam_pipeline(**params):
     name=params["name"] if "name" in params else config["required"]["name"]
@@ -67,7 +90,6 @@ def roam_pipeline(**params):
         sys.exit(1)
     #create Amsample object
     ams = a.Amsample(name=name, abbrev=abbrev)
-    gc = ""
     # eg: ams = a.Amsample(name="Ust_Ishim", abbrev="Ust")
     stages = params["stages"[:]] if "stages" in params else config["basic"]["stages"].split(",") 
     stage = stages[0]
@@ -80,14 +102,9 @@ def roam_pipeline(**params):
         if library == "strands" or not library:
             print("library is a required parameter")
             sys.exit(1)
-        lengths = params["lengths"] if "lengths" in params else config["basic"]["chr_lengths"].split(",")
-        lengths = [int(x) for x in lengths]
-        species = params["species"] if "species" in params else config["basic"]["species"]
         trim = params["trim"] if "trim" in params else config["basic"].getboolean("trim_ends")
-        chroms = params["chroms"] if "chroms" in params else config["basic"]["chroms"].split(",")
         mapq = int(params["mapq"]) if "mapq" in params else int(config["basic"]["mapq_thresh"])
         qual = int(params["qual"]) if "qual" in params else int(config["basic"]["qual_thresh"])
-        gc = params["gc_file"] if "gc_file" in params else config["files"]["gc_object"]
         
 #populate object from bam file
         ams.bam_to_am(filename=filename, library=library, chr_lengths=lengths, species=species, trim_ends=trim, chroms=chroms, filedir=filedir, file_per_chrom=file_per_chrom, mapq_thresh=mapq, qual_thresh=qual, gc_object=gc)
@@ -112,7 +129,6 @@ def roam_pipeline(**params):
                 #create Mmsample object
                 mms = m.Mmsample()
                 bismark = params["bismark"] if "bismark" in params else config["files"]["bismark_infile"]
-                gc = params["gc_file"] if "gc_file" in params else config["files"]["gc_object"]
                 modern = params["modern"] if "modern" in params else config["files"]["modern_infile"]
                 mod_name = params["mname"] if "mname" in params else config["modern"]["mod_name"]
                 mod_abbrev = params["mabbrev"] if "mabbrev" in params else config["modern"]["mod_abbrev"]
@@ -129,8 +145,7 @@ def roam_pipeline(**params):
                 ams.estimate_drate(ref=mms)
             elif stage == "meth":
                 ams.reconstruct_methylation(ref=mms)
-                gc = params["gc_file"] if "gc_file" in params else config["files"]["gc_object"]
-    
+                
     
     #dump object to text file
     outdir = params["outdir"] if "outdir" in params else config["paths"]["outdir"]
@@ -141,7 +156,7 @@ if filedir and not file_per_chrom:
     for fn in filenames:
         name = abbrev = fn.split("/")[-1].split(".")[0]
         #roam_pipeline(filename=fn, name=name, abbrev=abbrev, library=library)
-        roam_pipeline(**parameterss)
+        roam_pipeline(**parameters)
 else:
     #roam_pipeline(filename=fn, name=name, abbrev=abbrev, library=library)
     roam_pipeline(**parameters)
